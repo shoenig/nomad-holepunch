7e12b3006f21007fdd2b4c9d017abc5e2b620e09
Seth Hoenig <shoenig@duck.com>
2023-07-15 12:55:27 +0000

    config: log if there was an error loading config

--- configuration/env_test.go
+++ configuration/env_test.go
@@ -4,6 +4,7 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/shoenig/loggy"
 	"github.com/shoenig/test/must"
 )
 
@@ -13,29 +14,37 @@ func Test_defaultSocketPath(t *testing.T) {
 		envNomadSecretsDir     string
 		envHolepunchSocketPath string
 		exp                    string
-	}{{
-		name: "no env set",
-		exp:  "/secrets/api.sock",
-	}, {
-		name:               "only secrets dir set",
-		envNomadSecretsDir: "/alloc/secrets",
-		exp:                "/alloc/secrets/api.sock",
-	}, {
-		name:                   "only socket path set",
-		envHolepunchSocketPath: "/var/my.sock",
-		exp:                    "/var/my.sock",
-	}, {
-		name:                   "both env set",
-		envNomadSecretsDir:     "/alloc/secrets",
-		envHolepunchSocketPath: "/var/my.sock",
-		exp:                    "/var/my.sock",
-	}}
+	}{
+		{
+			name: "no env set",
+			exp:  "/secrets/api.sock",
+		},
+		{
+			name:               "only secrets dir set",
+			envNomadSecretsDir: "/alloc/secrets",
+			exp:                "/alloc/secrets/api.sock",
+		},
+		{
+			name:                   "only socket path set",
+			envHolepunchSocketPath: "/var/my.sock",
+			exp:                    "/var/my.sock",
+		},
+		{
+			name:                   "both env set",
+			envNomadSecretsDir:     "/alloc/secrets",
+			envHolepunchSocketPath: "/var/my.sock",
+			exp:                    "/var/my.sock",
+		},
+	}
+
+	log := loggy.New("test")
 
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
+			t.Setenv("NOMAD_TOKEN", "abc123")
 			t.Setenv("NOMAD_SECRETS_DIR", tc.envNomadSecretsDir)
 			t.Setenv("HOLEPUNCH_SOCKET_PATH", tc.envHolepunchSocketPath)
-			c := Load()
+			c := Load(log)
 			unixPath := filepath.ToSlash(c.SocketPath)
 			must.Eq(t, tc.exp, unixPath)
 		})
